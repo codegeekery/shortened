e0ac0edc986f20c09ebe87c6b85ea4b8c1092fc2
codegeekery <85375414+codegeekery@users.noreply.github.com>
2024-06-18 22:55:04 +0100

    new features

--- utils/endpoint.js
+++ utils/endpoint.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.NODE_ENV === 'production' ? 'https://shortenerurl.vercel.app' : 'http://localhost:3001';
+const API_URL = process.env.NODE_ENV === 'production' ? 'https://shortened-url-backend.vercel.app' : 'http://localhost:3001';
 
 export { API_URL}
