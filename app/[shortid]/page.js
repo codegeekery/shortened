338f6b69057cc60d33c4e645538a68ce34326641
codegeekery <85375414+codegeekery@users.noreply.github.com>
2024-06-29 21:22:25 +0100

    new fixes

--- app/[shortid]/page.js
+++ app/[shortid]/page.js
@@ -1,25 +1,33 @@
-'use client'
-import { redirect } from "next/navigation"
-import useSWR from "swr"
-import { API_URL } from "@/utils/endpoint"
-import { useParams } from "next/navigation"
-import {
-    Loader
-} from 'lucide-react'
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import useSWR from 'swr';
+import { API_URL } from '@/utils/endpoint';
+import { useParams } from 'next/navigation';
 
-const fetcher = (...args) => fetch(...args).then(res => res.json())
+// Fetcher function to be used with SWR
+const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 export default function Page() {
-    const { shortid } = useParams()
-    const { data, isLoading } = useSWR(`${API_URL}/GetShorter/${shortid}`, fetcher)
+    const { shortid } = useParams();
+    const router = useRouter();
+    const { data, error } = useSWR(`${API_URL}/GetShorter/${shortid}`, fetcher, { revalidateOnFocus: false });
 
-    if (isLoading) return (
-        <>
-            <div className="flex justify-center items-center h-screen">
-                <Loader size={32} />
-            </div>
-        </>
-    )
+    useEffect(() => {
+        if (data) {
+            if (data.id) {
+                // Use router.replace to prevent adding a new entry to the browser history
+                router.replace(data.id);
+            } else {
+                // Redirect to home if there's no valid redirect URL
+                router.replace('/');
+            }
+        } else if (error) {
+            // Redirect to home in case of an error
+            router.replace('/');
+        }
+    }, [data, error, router]);
 
-    redirect(data?.id)
+    // Return null since useEffect handles the redirection.
+    return null;
 }
--- utils/endpoint.js
+++ utils/endpoint.js
@@ -1,3 +1,4 @@
 const API_URL = process.env.NODE_ENV === 'production' ? 'https://shortened-url-backend.vercel.app' : 'http://localhost:3001';
+const HOME = process.env.NODE_ENV === 'production' ? 'https://shortened-url.vercel.app' : 'http://localhost:3000';
 
-export { API_URL}
+export { API_URL, HOME}
